3a5de213205eeed7b892031bed6760bb66c712f5
yeondi <yeondi.code@gmail.com>
2024-01-08 13:43:30 +0900

    updateProcess 수정

--- Dhere-boot/src/main/java/com/springbootstudy/dhere/service/MemberService.java
+++ Dhere-boot/src/main/java/com/springbootstudy/dhere/service/MemberService.java
@@ -5,7 +5,7 @@ import com.springbootstudy.dhere.domain.Member;
 public interface MemberService {
 	
 	// 로그인 
-	public abstract int login(String email,String pass);
+	public abstract int login(String email, String pass);
 
 	// 회원정보 가져오기
 	public abstract Member getMember(String email);
@@ -15,6 +15,9 @@ public interface MemberService {
 	
 	//아이디 중복확인 메서드
 	public abstract boolean overEmailCheck(String email);
+
+	// 비밀번호 체크 메서드
+	public abstract boolean memberPassCheck(String email, String pass);
 	
 
 
--- Dhere-boot/src/main/webapp/WEB-INF/views/member/mypageUpdateProcess.jsp
+++ Dhere-boot/src/main/webapp/WEB-INF/views/member/mypageUpdateProcess.jsp
@@ -4,6 +4,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 
+
 <link rel="stylesheet" href="resources/css/login.css">
 <html xmlns:th="http://www.thymeleaf.org">
    <head>
