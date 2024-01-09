9de81cd67693f88d8d214ce58f5b4e294d8617c2
youngjin <hdrhdr1@gmail.com>
2024-01-09 14:42:30 +0900

    asd

--- Dhere-boot/src/main/java/com/springbootstudy/dhere/controller/NoticeController.java
+++ Dhere-boot/src/main/java/com/springbootstudy/dhere/controller/NoticeController.java
@@ -7,12 +7,9 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,7 +24,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@Slf4j
 public class NoticeController {
 	
 	private static final String DEFAULT_PATH = "/resources/images/inquiry/";
